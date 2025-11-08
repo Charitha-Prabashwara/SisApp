f5d9258ed2a617fe8a24289d1370ae9233c54a1b
Charitha-Prabashwara <prabhashwara.seu@gmail.com>
2025-11-08 10:57:00 +0530

    auth page created and its working

---
+++ app/src/main/java/com/example/sisapp/Lock.java
@@ -0,0 +1,53 @@
+package com.example.sisapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Lock extends AppCompatActivity {
+
+    EditText password;
+
+    String passwordText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lock);
+
+
+        password = findViewById(R.id.passwordTextFiled);
+        passwordText = "12345";
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            if (password.getText().toString().equals(passwordText)){
+                Intent intent = new Intent(Lock.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/sisapp/MainActivity.java
+++ app/src/main/java/com/example/sisapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sisapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SPLASH_DELAY = 3000; // 3 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Auto-move to HomeActivity after 3 seconds
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, Lock.class);
+            startActivity(intent);
+            finish(); // so user can't go back to splash
+        }, SPLASH_DELAY);
     }
 }
